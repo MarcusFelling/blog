c010e6e3981b78c15788b2decf28e271db9ed588
Marcus Felling <marcus.felling@gmail.com>
2025-01-18 16:22:59 -0600

    update tests

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('should allow me to view landing page', async ({ page }) => {
   await page.goto('');
-  await expect(page.getByRole('link', { name: 'Marcus\' Blog' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'RSS' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'GitHub', exact: true })).toBeVisible();
   await expect(page.getByRole('link', { name: 'LinkedIn' })).toBeVisible();
